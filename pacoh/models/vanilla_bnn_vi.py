posterior bands, fixes in normalization mechanism

pacoh/models/vanilla_bnn_vi.py
@@ -10,6 +10,7 @@ from jax import numpy as jnp
 from tqdm import trange
 
 from pacoh.modules.abstract import RegressionModel
+from pacoh.modules.distributions import AffineTransformedDistribution
 from pacoh.modules.pure_functions import LikelihoodInterface, get_pure_batched_likelihood_functions, \
     get_pure_batched_nn_functions
 from pacoh.modules.priors_posteriors import GaussianBelief, GaussianBeliefState
@@ -79,6 +80,7 @@ def neg_elbo(posterior: Dict[str, GaussianBeliefState],
 
 class BayesianNeuralNetworkVI(RegressionModel):
     def __init__(self, input_dim: int, output_dim: int, normalize_data: bool = True,
+                 normalization_stats: Dict[str, np.array] = None,
                  random_state: jax.random.PRNGKey = None,
                  hidden_layer_sizes=(32, 32), activation=jax.nn.elu,
                  likelihood_std=0.1, learn_likelihood=True, prior_std=1.0, prior_weight=0.1,
@@ -94,7 +96,7 @@ class BayesianNeuralNetworkVI(RegressionModel):
         :param learn_likelihood: Whether to interpret the likelihood std as a learnable variable with a mean and std itself
         :param prior_std: The std of the prior on each NN weight
         :param prior_weight: Multiplicative factor before the KL divergence term in the ELBO
-        :param likelihood_prior_mean: TODO figure out why this is not the same thing as the likelihood std
+        :param likelihood_prior_mean:
         :param likelihood_prior_std: The sigma in sigmall sim N(0,sigma)
         :param batch_size_vi: The number of samples from the posterior to approximate the expectation in the ELBO
         :param batch_size: The number of data points you get while training and predicting
@@ -106,6 +108,7 @@ class BayesianNeuralNetworkVI(RegressionModel):
         self.likelihood_std = likelihood_std
         self.batch_size = batch_size
         self.batch_size_vi = batch_size_vi
+        self._set_normalization_stats(normalization_stats)
 
         """ A) Get batched forward functions for the nn and likelihood"""
         keys = jax.random.split(self._rds, self.batch_size_vi + 1)
@@ -115,7 +118,6 @@ class BayesianNeuralNetworkVI(RegressionModel):
                                                                                         hidden_layer_sizes,
                                                                                         activation)
 
-        # if non learnable, this should be a REAL initialization TODO
         batched_likelihood_init_fn, self.batched_likelihood_apply_fns, self.batched_likelihood_apply_fns_batch_inputs = get_pure_batched_likelihood_functions(likelihood_prior_mean)
 
         """ B) Get dummy pytrees for the different module parameters => TODO somehow abstract this inside some utils. """
@@ -134,19 +136,17 @@ class BayesianNeuralNetworkVI(RegressionModel):
             'nn': nn_prior_state,
         }
         self.posterior = {
-            'nn': nn_prior_state.copy(),
+            'nn': nn_prior_state,
         }
 
         self.learn_likelihood = learn_likelihood
         if learn_likelihood:
             lh_prior_state = GaussianBeliefState.initialize(likelihood_prior_mean, likelihood_prior_std, likelihood_param_template)
             self.prior['lh'] = lh_prior_state
-            self.posterior['lh'] = lh_prior_state.copy()
+            self.posterior['lh'] = lh_prior_state
             self.fixed_likelihood_params = None
         else:
             self.fixed_likelihood_params = likelihood_params
-            # TODO make sure this gets initialized as desired
-        # TODO understand why Jonas initializes the prior differently for learnable and non-learnable likelihood?
 
         """ D) Setup optimizer: posterior parameters """
         lr_scheduler = optax.constant_schedule(lr)
@@ -208,29 +208,23 @@ class BayesianNeuralNetworkVI(RegressionModel):
         self._rds, sampler_key = jax.random.split(self._rds)
         return Sampler(xs, ys, batch_size, sampler_key)
 
-    def predict(self, xs, num_posterior_samples=20):
+    def predict(self, xs, num_posterior_samples=20, return_density=True):
         # a) data handling
         xs = _handle_batch_input_dimensionality(xs)
-        warnings.warn("normalize")
-        # xs = self._normalize_data(xs)
+        xs = self._normalize_data(xs)
 
         # b) nn prediction
         self._rds, nn_key, lh_key = jax.random.split(self._rds, 3)
 
         nn_params = GaussianBelief.rsample(self.posterior['nn'], nn_key, num_posterior_samples)
-        nn_shape = pytree_shape(nn_params)
-
 
         ys_pred = self.batched_nn(nn_params, None, xs)
-        mean_shape = ys_pred.shape
 
         if self.learn_likelihood:
             lh_params = GaussianBelief.rsample(self.posterior['lh'], lh_key, num_posterior_samples)
         else:
             # lh_params = self.fixed_likelihood_params # there are not enough here
             lh_params = broadcast_params(pytree_unstack(self.fixed_likelihood_params, 1)[0], num_posterior_samples)
-
-        lh_shape = pytree_shape(lh_params)
 
         pred_dists = self.batched_likelihood_apply_fns_batch_inputs.get_posterior_from_means(lh_params, None, ys_pred)
         # shape is (num_posterior_samples, batch_size, output_dim) -> transformed to (batch_size, output_dim, num_posterior_samples)
@@ -241,18 +235,23 @@ class BayesianNeuralNetworkVI(RegressionModel):
 
         pred_mixture = numpyro.distributions.MixtureSameFamily(mixture, pred_dists)
 
-        # TODO handle
-        #ys_pred = self._unnormalize_preds(ys_pred)  # should that be some kind of mean??
-        #pred_dist = self._unnormalize_predictive_dist(pred_mixture)
+        pred_mixture_transformed = AffineTransformedDistribution(pred_mixture,
+                                                                 normalization_mean=self.y_mean,
+                                                                 normalization_std=self.y_std)
+        output_normal_dist = self._affine_transformed_distribution_to_normal(pred_mixture_transformed)
 
-        return jnp.mean(ys_pred, axis=0), pred_mixture
+        if return_density:
+            return output_normal_dist
+        else:
+            return output_normal_dist.mean, output_normal_dist.scale
+
 
 
     def _step(self, x_batch, y_batch):
         self._rds, step_key = jax.random.split(self._rds)
         nelbo, gradelbo = self.elbo_val_and_grad(self.posterior, step_key, x_batch, y_batch, num_train_points=self._num_train_points)
         updates, new_opt_state = self.optimizer.update(gradelbo, self.optimizer_state, self.posterior)
-        self.optimizer_state = new_opt_state # WARNING this was wrong before
+        # self.optimizer_state = new_opt_state # WARNING this was wrong before
         self.posterior = optax.apply_updates(self.posterior, updates)
         return nelbo
 
@@ -272,21 +271,21 @@ if __name__ == '__main__':
     x_plot = np.expand_dims(x_plot, -1)
     y_val = np.sin(x_plot) + np.random.normal(scale=0.1, size=x_plot.shape)
 
-    nn = BayesianNeuralNetworkVI(input_dim=d, output_dim=1, hidden_layer_sizes=(32, 32), prior_weight=0.001,
+    nn = BayesianNeuralNetworkVI(input_dim=d, output_dim=1, batch_size_vi=10, hidden_layer_sizes=(32, 32), prior_weight=0,
                                  learn_likelihood=True)
     nn.add_data_points(x_train, y_train)
 
     n_iter_fit = 2000  # 2000
     for i in range(10):
-        nn.fit(log_period=10, num_iter_fit=n_iter_fit)
+        nn.fit(log_period=100, num_iter_fit=n_iter_fit)
         from matplotlib import pyplot as plt
 
-        pred, pred_mixture = nn.predict(x_plot)
-
-        ucb = pred + pred_mixture.variance
-        lcb = pred - pred_mixture.variance
+        pred, out_dist = nn.predict(x_plot)
+        lcb, ucb = nn.confidence_intervals(x_plot)
+        # ucb = out_dist.mean + out_dist.variance
+        # lcb = out_dist.mean - out_dist.variance
         plt.fill_between(x_plot.flatten(), lcb.flatten(), ucb.flatten(), alpha=0.3)
-        plt.plot(x_plot, pred_mixture.mean)
+        plt.plot(x_plot, out_dist.mean)
         plt.scatter(x_train, y_train)
 
         plt.show()

pacoh/models/vanilla_gp.py
@@ -4,11 +4,11 @@ import jax.random
 import numpy as np
 import numpyro.distributions
 
-from pacoh.modules import JAXExactGP, JAXConstantMean
+from pacoh.modules.gp_lib import JAXExactGP, JAXConstantMean
 from pacoh.modules.kernels import JAXRBFKernel
 from pacoh.modules.distributions import AffineTransformedDistribution, JAXGaussianLikelihood
 from pacoh.modules.abstract import RegressionModel
-from pacoh.util import _handle_batch_input_dimensionality
+from pacoh.util.data_handling import _handle_batch_input_dimensionality
 from typing import Dict, NamedTuple, Any
 import haiku as hk
 
@@ -69,10 +69,14 @@ class GPRegressionVanilla(RegressionModel):
                                                    kernel_lengthscale,
                                                    likelihood_variance)
 
-        init_fn, self._apply_fns = hk.multi_transform_with_state(factory)
+        self._init_fn, self._apply_fns = hk.multi_transform_with_state(factory)
         self._rds, init_key = jax.random.split(self._rds)
         # initialize parameters using some dummy data
-        self._params, self._state = init_fn(init_key, jnp.ones((1, input_dim)))
+        self._params, self._state = self._init_fn(init_key, jnp.ones((1, input_dim)))
+
+        # keep these around to reset to prior and evaluate prior
+        self._prior_params = self._params
+        self._prior_state = self._state
 
     def predict(self, test_x, return_density=False, **kwargs):
         """
@@ -92,6 +96,7 @@ class GPRegressionVanilla(RegressionModel):
         test_x_normalized = self._normalize_data(test_x)
         self._rds, predict_key = jax.random.split(self._rds)
         pred_dist, self._state = self._apply_fns.pred_dist_fn(self._params, self._state, predict_key, test_x_normalized)
+
         pred_dist_transformed = AffineTransformedDistribution(pred_dist,
                                                               normalization_mean=self.y_mean,
                                                               normalization_std=self.y_std)
@@ -103,9 +108,7 @@ class GPRegressionVanilla(RegressionModel):
             return pred_mean, pred_std
 
     def reset_to_prior(self):
-        raise NotImplementedError
-        self._reset_data()
-        self.gp.reset_to_prior()
+        self._params, self._state = self._prior_params, self._prior_state
 
     def _recompute_posterior(self):
         """Fits the underlying GP to the currently stored datapoints. """
@@ -121,26 +124,33 @@ class GPRegressionVanilla(RegressionModel):
         Returns:
             The prior distributions
         """
-        return self._prior_fn(xs)
+        self._normalize_data(xs)
+        self._rds, predict_key = jax.random.split(self._rds)
+        pred_dist, _ = self._apply_fns.pred_dist_fn(self._prior_params, self._prior_state, predict_key, xs)
 
+        pred_dist_transformed = AffineTransformedDistribution(pred_dist,
+                                                              normalization_mean=self.y_mean,
+                                                              normalization_std=self.y_std)
 
-    # def predict_mean_std(self, test_x):
-    #     # do we really need this?
-    #     return self.predict(test_x, return_density=False)
+        return pred_dist_transformed
 
-    def state_dict(self):
-        # TODO rename
-        state_dict = {
-            'model': self.gp.state_dict(),
-        }
-        return state_dict
+    def predict_mean_std(self, test_x):
+        return self.predict(test_x, return_density=False)
 
-    def load_state_dict(self, state_dict):
-        self.gp.load_state_dict(state_dict['model'])
+    # def state_dict(self):
+    #     # TODO rename
+    #     state_dict = {
+    #         'model': self.gp.state_dict(),
+    #     }
+    #     return state_dict
+    #
+    # def load_state_dict(self, state_dict):
+    #     self.gp.load_state_dict(state_dict['model'])
 
-    def _vectorize_pred_dist(self, pred_dist):
-        # this is because ultimately, we do not want a TransformedDistribution object
-        return numpyro.distributions.Normal(pred_dist.mean.flatten(), pred_dist.stddev.flatten())
+    # def _vectorize_pred_dist(self, pred_dist):
+    #     print("this is called")
+    #     # this is because ultimately, we do not want a TransformedDistribution object
+    #     return numpyro.distributions.Normal(pred_dist.mean.flatten(), pred_dist.stddev.flatten())
 
 
 if __name__ == "__main__":
@@ -162,17 +172,23 @@ if __name__ == "__main__":
     x_data_train, x_data_test = x_data[:n_train_samples].numpy(), x_data[n_train_samples:].numpy()
     y_data_train, y_data_test = y_data[:n_train_samples].numpy(), y_data[n_train_samples:].numpy()
 
-    gp_mll = GPRegressionVanilla(input_dim=x_data.shape[-1])
-    gp_mll.add_data(x_data_train, y_data_train)
+    gp = GPRegressionVanilla(input_dim=x_data.shape[-1])
+    gp.add_data(x_data_train, y_data_train)
 
     x_plot = np.linspace(6, -6, num=n_test_samples)
 
-    pred_dist = gp_mll.predict(x_plot, return_density=True)
-    pred_mean, pred_std = pred_dist.mean, pred_dist.stddev
-    lcb, ucb = pred_mean-pred_std, pred_mean + pred_std
-    lcb, ucb = lcb.flatten(), ucb.flatten()
+    pred_dist = gp.predict(x_plot, return_density=True)
+    lcb, ucb = gp.confidence_intervals(x_plot, confidence=0.9)
+    # pred_dist = gp._affine_transformed_distribution_to_normal(pred_dist)
+    pred_mean, pred_std = pred_dist.mean, pred_dist.scale
+    # lcb, ucb = pred_mean-pred_std, pred_mean + pred_std
+    # lcb, ucb = lcb.flatten(), ucb.flatten()
     plt.plot(x_plot, pred_mean)
-    plt.fill_between(x_plot, lcb, ucb, alpha=0.4)
+    plt.fill_between(x_plot, lcb.flatten(), ucb.flatten(), alpha=0.4)
+
+    gp.reset_to_prior()
+    pred_mean, _ = gp.predict(x_plot, return_density=False)
+    plt.plot(x_plot, pred_mean)
 
     plt.scatter(x_data_test, y_data_test)
     plt.scatter(x_data_train, y_data_train)

pacoh/modules/abstract.py
@@ -2,6 +2,8 @@ import warnings
 from typing import Union
 
 import numpy as np
+from numpyro.distributions import Normal
+
 from pacoh.util.data_handling import _handle_batch_input_dimensionality, _handle_point_input_dimensionality
 from config import device
 
@@ -29,8 +31,8 @@ class RegressionModel(ABC):
         """
         self.input_dim = input_dim
         self.normalize_data = normalize_data
-        self.xs_data = jnp.zeros((0, self.input_dim), dtype=np.double)
-        self.ys_data = jnp.zeros((0,), dtype=np.double)
+        self.xs_data = jnp.zeros((0, self.input_dim))
+        self.ys_data = jnp.zeros((0,))
         self._num_train_points = 0
         self._rds = random_state if random_state is not None else jax.random.PRNGKey(42)
 
@@ -63,8 +65,7 @@ class RegressionModel(ABC):
         assert xs.ndim == 2 and xs.shape[1] == self.input_dim
         # handle input dimensionality and normalize data
         xs, ys = _handle_batch_input_dimensionality(xs, ys)
-        #xs, ys = self._normalize_data(xs, ys)
-        warnings.warn("undo")
+        xs, ys = self._normalize_data(xs, ys)
         # concatenate to new datapoints
         self.xs_data = np.concatenate([self.xs_data, xs])
         self.ys_data = np.concatenate([self.ys_data, ys])
@@ -119,11 +120,12 @@ class RegressionModel(ABC):
 
     def confidence_intervals(self, test_x, confidence=0.9, **kwargs):
         pred_dist = self.predict(test_x, return_density=True, **kwargs)
-        pred_dist = self._vectorize_pred_dist(pred_dist)
+        pred_dist = self._affine_transformed_distribution_to_normal(pred_dist)
         alpha = (1 - confidence) / 2
-        ucb = pred_dist.icdf(np.ones(test_x.size) * (1 - alpha))
-        lcb = pred_dist.icdf(np.ones(test_x.size) * alpha)
-        return ucb, lcb
+        ucb = pred_dist.icdf((1 - alpha)*jnp.ones(pred_dist.batch_shape))
+        lcb = pred_dist.icdf(alpha*jnp.ones(pred_dist.batch_shape))
+        return lcb, ucb
+
 
     """ ----- Private methods ----- """
     @abstractmethod
@@ -156,7 +158,6 @@ class RegressionModel(ABC):
             self.y_std = normalization_stats_dict['y_std'].squeeze()
 
     def _calib_error(self, pred_dist_vectorized, test_t_tensor):
-        raise NotImplementedError
         return _calib_error(pred_dist_vectorized, test_t_tensor)
 
     def _normalize_data(self, xs, ys=None):
@@ -178,16 +179,49 @@ class RegressionModel(ABC):
         """
         # save mean and variance of data for normalization
         if self.normalize_data:
-            self.x_mean, self.y_mean = jnp.mean(X, axis=0), jnp.mean(Y, axis=0)
-            self.x_std, self.y_std = jnp.std(X, axis=0) + 1e-8, jnp.std(Y, axis=0) + 1e-8
+            self.x_mean, self.y_mean = jnp.mean(xs, axis=0), jnp.mean(ys, axis=0)
+            self.x_std, self.y_std = jnp.std(xs, axis=0) + 1e-8, jnp.std(ys, axis=0) + 1e-8
         else:
-            self.x_mean, self.y_mean = jnp.zeros(X.shape[1]), jnp.zeros(Y.shape[1])
-            self.x_std, self.y_std = jnp.ones(X.shape[1]), jnp.ones(Y.shape[1])
+            self.x_mean, self.y_mean = jnp.zeros(xs.shape[1]), jnp.zeros(ys.shape[1])
+            self.x_std, self.y_std = jnp.ones(xs.shape[1]), jnp.ones(ys.shape[1])
 
+
+    # def _unnormalize_pred(self, pred_mean, pred_std):
+    #     assert hasattr(self, "x_mean") and hasattr(self, "x_std"), "requires computing normalization stats beforehand"
+    #     assert hasattr(self, "y_mean") and hasattr(self, "y_std"), "requires computing normalization stats beforehand"
+    #
+    #     if self.normalize_data:
+    #         assert pred_mean.ndim == pred_std.ndim == 2 and pred_mean.shape[1] == pred_std.shape[1] == self.output_dim
+    #         if isinstance(pred_mean, torch.Tensor) and isinstance(pred_std, torch.Tensor):
+    #             y_mean_tensor, y_std_tensor = torch.tensor(self.y_mean).float(), torch.tensor(self.y_std).float()
+    #             pred_mean = pred_mean.mul(y_std_tensor[None, :]) + y_mean_tensor[None, :]
+    #             pred_std = pred_std.mul(y_std_tensor[None, :])
+    #         else:
+    #             pred_mean = pred_mean.multiply(self.y_std[None, :]) + self.y_mean[None, :]
+    #             pred_std = pred_std.multiply(self.y_std[None, :])
+    #
+    #     return pred_mean, pred_std
+    #
+    # def _initial_data_handling(self, train_x, train_t):
+    #     train_x, train_t = _handle_input_dimensionality(train_x, train_t)
+    #     self.input_dim, self.output_dim = train_x.shape[-1], train_t.shape[-1]
+    #     self.n_train_samples = train_x.shape[0]
+    #
+    #     # b) normalize data to exhibit zero mean and variance
+    #     self._compute_normalization_stats(train_x, train_t)
+    #     train_x_normalized, train_t_normalized = self._normalize_data(train_x, train_t)
+    #
+    #     # c) Convert the data into pytorch tensors
+    #     self.train_x = torch.from_numpy(train_x_normalized).contiguous().float().to(device)
+    #     self.train_t = torch.from_numpy(train_t_normalized).contiguous().float().to(device)
+
+        return self.train_x, self.train_t
     # def _vectorize_pred_dist(self, pred_dist: numpyro.distributions.Distribution) -> numpyro.distributions.Distribution:
     #     warnings.warn("check all use cases her")
     #     return numpyro.distributions.Normal(loc=pred_dist.mean, scale=jnp.sqrt(pred_dist.variance))
 
+    def _affine_transformed_distribution_to_normal(self, transformed_dist):
+        return Normal(loc=transformed_dist.mean, scale=transformed_dist.stddev)
 
 class RegressionModelMetaLearned(RegressionModel, ABC):
     """
@@ -295,11 +329,11 @@ class RegressionModelMetaLearned(RegressionModel, ABC):
     #     """
     #     return numpyro.distributions.Normal(pred_dist.mean, pred_dist.scale)
 
-def _calib_error(pred_dist_vectorized, test_t_tensor):
-    cdf_vals = pred_dist_vectorized.cdf(test_t_tensor)
-    
+def _calib_error(pred_dist_vectorized, test_ys):
+    cdf_vals = pred_dist_vectorized.cdf(test_ys)
+
     if test_t_tensor.shape[0] == 1:
-        test_t_tensor = test_t_tensor.flatten()
+        test_t_tensor = test_ys.flatten()
         cdf_vals = cdf_vals.flatten()
 
     num_points = test_t_tensor.shape[0]

pacoh/modules/common.py
@@ -8,7 +8,10 @@ class PositiveParameter(hk.Module):
         super().__init__(name=name)
         self.raw_init = jnp.log(initial_value - boundary_value)
         self.boundary_value = boundary_value
-        self.shape = initial_value.shape
+        if isinstance(initial_value, float):
+            self.shape = []
+        else:
+            self.shape = initial_value.shape
 
     def __call__(self):
         exponentiated = jnp.exp(hk.get_parameter("__positive_log_scale_param", dtype=jnp.float32, shape=self.shape,

pacoh/modules/distributions.py
@@ -1,10 +1,6 @@
-import warnings
-
-import numpyro.distributions
-from numpyro.distributions import Distribution, Normal
+from numpyro.distributions import Normal
 from numpyro.distributions import TransformedDistribution
 from numpyro.distributions.transforms import AffineTransform
-import torch
 import numpy as np
 from jax import numpy as jnp
 import haiku as hk
@@ -25,8 +21,7 @@ class AffineTransformedDistribution(TransformedDistribution):
     """
 
     def __init__(self, base_dist, normalization_mean, normalization_std):
-        # self.loc_tensor = torch.tensor(normalization_mean).float().reshape((1,))
-        self.scale = normalization_std.reshape((1,))
+        self.norm_scale = normalization_std.reshape((1,)) # so we can apply it on batches
         normalization_transform = AffineTransform(loc=normalization_mean, scale=normalization_std)
         super().__init__(base_dist, normalization_transform)
 
@@ -36,17 +31,21 @@ class AffineTransformedDistribution(TransformedDistribution):
 
     @property
     def stddev(self):
-        return np.exp(np.log(self.base_dist.scale) + np.log(self.scale))
+        if hasattr(self.base_dist, "scale"):
+            return np.exp(np.log(self.base_dist.scale) + np.log(self.norm_scale))
+        elif hasattr(self.base_dist, "variance"):
+            return np.exp(0.5*np.log(self.base_dist.variance) + np.log(self.norm_scale))
+        elif hasattr(self.base_dist, "stddev"):
+            return np.exp(np.log(self.base_dist.stddev) + np.log(self.norm_scale))
 
     @property
     def variance(self):
-        return np.exp(np.log(self.base_dist.variance) + 2 * np.log(self.scale))
+        return np.exp(np.log(self.base_dist.variance) + 2 * np.log(self.norm_scale))
 
 
 class JAXGaussianLikelihood(hk.Module):
     def __init__(self, variance: float = 1.0, variance_constraint_gt=0.0, output_dim=1):
         super().__init__()
-        # TODO, to avoid numerical errors, I should store the std, not the variance, no
         variance = jnp.ones((output_dim,))*variance
         self.variance = PositiveParameter(variance, boundary_value=variance_constraint_gt)
 

pacoh/modules/gp_lib.py
@@ -2,8 +2,8 @@ import functools
 
 import jax.lax.linalg
 import numpyro.distributions
-from abc import ABC, abstractmethod
-from jax import numpy as jnp, vmap
+from abc import abstractmethod
+from jax import numpy as jnp
 from jax.scipy.linalg import cho_solve, cho_factor
 import haiku as hk
 
@@ -65,13 +65,13 @@ class JAXExactGP:
             mean = self.mean_module(x) + jnp.dot(new_cov_row, cho_solve((chol, True), self._ys_centered(xs, ys)))
             var = self.cov_vec_vec(x, x) - jnp.dot(new_cov_row, cho_solve((chol, True), new_cov_row))
             std = jnp.sqrt(var)
-            return numpyro.distributions.Normal(loc=mean, scale=std)
+            return numpyro.distributions.Normal(loc=mean, scale=std) # TODO figure out batch_shape and event_shape
 
         def no_data(operand):
             x, _, __ = operand
             return self._prior(x)
 
-        # TODO warning: this is quite confusing. I had this as a jax.lax.cond/hk.cond, but because the distinction
+        # warning: this is quite confusing. I had this as a jax.lax.cond/hk.cond, but because the distinction
         # is on the size of xs, we cannot use these constructs, because they exactly assume that the size of
         # the inputs of both branches is the same
         if xs.size > 0:
@@ -132,7 +132,7 @@ class JAXConstantMean(JAXMean):
         # works for both batch or unbatched
         mean = hk.get_parameter("mu", shape=[], dtype=jnp.float32,
                                 init=hk.initializers.Constant(self.init_constant))
-        return jnp.ones(x.shape, dtype=jnp.float64) * mean
+        return jnp.ones(x.shape) * mean
 
 
 class JAXZeroMean(JAXMean):